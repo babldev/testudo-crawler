cc56b1fafb4ddbdb0e648dfb50cd543d75621cd6
Brady Law <bradyl16@gmail.com>
2011-01-16 15:57:15 -0500

    Almost done

--- testudo_crawl.py
+++ testudo_crawl.py
@@ -12,6 +12,15 @@ import os
 import re
 import urllib
 import unittest
+import logging
+
+logger = logging.getLogger('testudo_crawler')
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+ch.setFormatter(formatter)
+logger.addHandler(ch)
 
 class testudocrawler:
     base_url = 'http://www.sis.umd.edu/bin/soc'
@@ -29,10 +38,14 @@ class testudocrawler:
     def get_departments(self):
         response = self.fetch_departments_page()
         pattern = re.compile('<a href=soc.*>(.*)</a>(.*)<br>', re.I)
-        parsed = list()
+        departments = list()
         for match in pattern.finditer(response):
-            parsed.append(dict(code=match.group(1).strip(), title=match.group(2).strip()))
-        return parsed
+            departments.append(dict(code=match.group(1).strip(), title=match.group(2).strip()))
+        
+        if self.verbose:
+            logger.info('%d departments found.' % len(departments))
+            
+        return departments
     
     """
     Returns a LIST of DICTIONARIES of the form:
@@ -49,17 +62,29 @@ class testudocrawler:
         skills such as program design and testing as well as implementation of programs using a \
         graphical IDE. Programming done in Java. '
     
-    'building' : 'CSI',
-    'room' : 2117,
-    'time_start' : '10:00am',
-    'time_end' : '10:50am',
-    'days' : 'MWF',
+    'c1_building' : 'CSI',
+    'c1_room' : 2117,
+    'c1_time_start' : '10:00am',
+    'c1_time_end' : '10:50am',
+    'c1_days' : 'MWF',
+    'c1_type' : None,
     
-    'dis_building' : 'CSI'
-    'dis_room' : 2120,
-    'dis_time_start' : '11:00am',
-    'dis_time_end' : '11:50am',
-    'dis_days' : 'MW',
+    'c2_building' : 'CSI',
+    'c2_room' : 2120,
+    'c2_time_start' : '11:00am',
+    'c2_time_end' : '11:50am',
+    'c2_days' : 'MW',
+    'c2_type' : 'Dis',
+    
+    'c3_building' : None,
+    'c3_room' : None,
+    'c3_time_start' : None,
+    'c3_time_end' : None,
+    'c3_days' : None,
+    'c3_type' : None,
+    
+    'start_date' : None, # Example '03/28/11'
+    'end_date' : None, # Example '03/28/11'
     
     'seats' : 25,
     'open' : 0,
@@ -67,9 +92,40 @@ class testudocrawler:
     }
     """
     def get_courses(self, dept):
+        if self.verbose:
+            logger.info('Downloading %s...' % (dept))
+            
         response = self.fetch_courses_page(dept=dept)
-        return None
-        
+        pattern = re.compile(r"""
+                <font\sface="arial,helvetica"\ssize=-1>[\s]*
+                <b>(?P<code>.*)<\/b>[\s]*
+                (<i>(?P<permreq>.*)<\/i>[\s]*)?                     # optional
+                <b>(?P<title>.*);<\/b>[\s]*
+                <b>\s*\((?P<credits>.*)\s+credits?\)\s*</b>[\s]*
+                Grade\s*Method:\s*(?P<grade_method>.*)\.[\s]*
+                <br>[\s]*
+                (<i>(?P<requirements>[^<]*)<\/i>[\s]*)?           # optional
+                (?P<description>[^<]*)
+                <\/font>[\s]*
+                """,
+                re.IGNORECASE | re.VERBOSE)
+        courses = list()
+        for m in pattern.finditer(response):
+            courses.append(dict(
+                    code=clean_and_trim(m.group('code')),
+                    title=clean_and_trim(m.group('title')),
+                    permreq=clean_and_trim(m.group('permreq')),
+                    credits=clean_and_trim(m.group('credits')),
+                    grade_method=clean_and_trim(m.group('grade_method')),
+                    requirements=clean_and_trim(m.group('requirements')),
+                    description=clean_and_trim(m.group('description'))
+                    ))
+
+        if self.verbose:
+            logger.info('%d courses downloaded for %s.' % (len(courses), dept))
+            
+        return courses
+            
     def fetch_departments_page(self):
         return self.fetch_courses_page(dept='DEPT')
         
@@ -79,7 +135,29 @@ class testudocrawler:
         response = f.read()
         f.close()
         return response
-
+    
+    def get_all_courses(self):
+        departments = self.get_departments()
+        all_courses = list()
+        
+        d_count = len(departments)
+        d_pos = 0
+        
+        for d in departments:
+            d_pos += 1
+            logger.info('Dept %d/%d' % (d_pos, d_count))
+            all_courses.extend(self.get_courses(dept=d['code']))
+            
+        if self.verbose:
+            logger.info('Done! %d courses found for %d departments.' % (len(all_courses), len(departments)))
+            
+        return all_courses
+        
+def clean_and_trim(string):
+    if string:
+        return string.replace('\n', ' ').strip()
+    else:
+        return None
 
 class testudocrawler_tests(unittest.TestCase):
     def setUp(self):
@@ -107,8 +185,30 @@ class testudocrawler_tests(unittest.TestCase):
     def test_get_courses(self):
         courses = self.crawler.get_courses('CMSC')
         assert courses is not None
-        assert len(courses) > 20
-        assert dict(code='CMSC', title='Computer Science') in courses
-
+        found = False
+        correct_course = {
+        'code': 'CMSC131',
+        'requirements': 'Corequisite: MATH140 and permission of department. Not open to students who have completed CMSC114.',
+        'description': 'Introduction to programming and computer science. Emphasizes understanding and implementation of applications using object-oriented techniques. Develops skills such as program design and testing as well as implementation of programs using a graphical IDE. Programming done in Java.',
+        'title': 'Object-Oriented Programming I',
+        'grade_method': 'REG',
+        'credits': '4',
+        'permreq': '(PermReq)'}
+        
+        for c in courses:
+            logger.debug(c)
+            
+            if c['code'] == correct_course['code']:
+                found = True
+                assert len(correct_course) == len(c)
+                for k, v in correct_course.items():
+                    assert c[k] == v, 'Course check failed!\nGenerated:\t%s\nCorrect:\t%s' % (c[k], v)
+            assert len(courses) > 20
+        assert found
+    
+    def test_get_all_courses(self):
+        courses = self.crawler.get_all_courses()
+        logger.debug(courses)
+        
 if __name__ == "__main__":
     unittest.main()
